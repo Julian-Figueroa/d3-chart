78e54d8a6dd4823d5b3f031e5c18a742d01197f3
Julian Figueroa <julianfigueroatorres@gmail.com>
2019-08-30 12:10:03 -0500

    Adding a funtion to update values depending on the Snapshot changes in FB

--- index.js
+++ index.js
@@ -27,62 +27,105 @@ const xAxisGroup = graph
   .attr('transform', `translate(0,${graphHeight})`);
 const yAxisGroup = graph.append('g');
 
-db.collection('dishes')
-  .get()
-  .then(res => {
-    let data = [];
-    res.docs.map(doc => {
-      data.push(doc.data());
-    });
+//Scales & Setup
+const y = d3.scaleLinear().range([graphHeight, 0]);
 
-    const max = d3.max(data, d => d.orders);
-    // const min = d3.min(data, d => d.orders);
-    // const extent = d3.extent(data, d => d.orders);
+const x = d3
+  .scaleBand()
+  .range([0, graphWidth])
+  .paddingInner(0.2)
+  .paddingOuter(0.2);
 
-    //scalling
-    const y = d3
-      .scaleLinear()
-      .domain([0, max])
-      .range([graphHeight, 0]);
+// //Create axis
+const xAxis = d3.axisBottom(x);
+const yAxis = d3
+  .axisLeft(y)
+  .ticks(3)
+  .tickFormat(d => d + ' orders');
 
-    const x = d3
-      .scaleBand()
-      .domain(data.map(item => item.name))
-      .range([0, graphWidth])
-      .paddingInner(0.2)
-      .paddingOuter(0.2);
+// Update x axis text
+xAxisGroup
+  .selectAll('text')
+  .attr('transform', 'rotate(-40)')
+  .attr('text-anchor', 'end')
+  .attr('fill', 'orange');
 
-    const rects = graph.selectAll('rect').data(data);
+//Update function
+const update = data => {
+  // updating scales
+  const max = d3.max(data, d => d.orders);
+  y.domain([0, max]);
+  x.domain(data.map(item => item.name));
 
-    rects
-      .attr('width', x.bandwidth)
-      .attr('height', d => graphHeight - y(d.orders))
-      .attr('fill', 'orange')
-      .attr('x', d => x(d.name))
-      .attr('y', d => y(d.orders));
+  // join the data to rects
+  const rects = graph.selectAll('rect').data(data);
 
-    rects
-      .enter()
-      .append('rect')
-      .attr('width', x.bandwidth)
-      .attr('height', d => graphHeight - y(d.orders))
-      .attr('fill', 'orange')
-      .attr('x', d => x(d.name))
-      .attr('y', d => y(d.orders));
+  // Remove exit selection
+  rects.exit().remove();
 
-    //Create axis
-    const xAxis = d3.axisBottom(x);
-    const yAxis = d3
-      .axisLeft(y)
-      .ticks(3)
-      .tickFormat(d => d + ' orders');
+  // update curretn shapes in DOM
+  rects
+    .attr('width', x.bandwidth)
+    .attr('height', d => graphHeight - y(d.orders))
+    .attr('fill', 'orange')
+    .attr('x', d => x(d.name))
+    .attr('y', d => y(d.orders));
 
-    xAxisGroup.call(xAxis);
-    yAxisGroup.call(yAxis);
+  //Enter, append the enter selection to the DOM
+  rects
+    .enter()
+    .append('rect')
+    .attr('width', x.bandwidth)
+    .attr('height', d => graphHeight - y(d.orders))
+    .attr('fill', 'orange')
+    .attr('x', d => x(d.name))
+    .attr('y', d => y(d.orders));
 
-    xAxisGroup
-      .selectAll('text')
-      .attr('transform', 'rotate(-40)')
-      .attr('text-anchor', 'end')
-      .attr('fill', 'orange');
+  // Calling Axis
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
+};
+
+let data = [];
+
+db.collection('dishes').onSnapshot(res => {
+  res.docChanges().map(change => {
+    // console.log(change.doc.data());
+    const doc = { ...change.doc.data(), id: change.doc.id };
+    switch (change.type) {
+      case 'added':
+        data.push(doc);
+        break;
+
+      case 'modified':
+        const index = data.findIndex(item => item.id === doc.id);
+        data[index] = doc;
+        break;
+
+      case 'removed':
+        data = data.filter(item => item.id !== doc.id);
+        break;
+
+      default:
+        break;
+    }
   });
+
+  //Calling the update function
+  update(data);
+  // .get()
+  // .then(res => {
+  //   let data = [];
+  //   res.docs.map(doc => {
+  //     data.push(doc.data());
+  //   });
+
+  //   //Calling the update function
+  //   update(data);
+
+  //Interval
+  // d3.interval(() => {
+  //   data[0].orders += 50;
+  //   // update(data);
+  // }, 1000);
+});
